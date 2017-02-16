ae92a9b283ce204261371d1f2fba970a917f3080
Anthony Fleury <anthony.fleury@outlook.com>
2017-02-16 16:05:38 -0200

    rake working

--- Gemfile
+++ Gemfile
@@ -57,4 +57,4 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-
+gem 'rails-controller-testing'
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,27 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
-  def show
-  end
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+     redirect_to cocktail_path(params[:cocktail_id])
+   else
+     render :new
+   end
+ end
+
+ def destroy
+  @dose = Dose.find(params[:id])
+  @dose.destroy 
+  redirect_to cocktail_path
+end
+private
+
+def dose_params   
+ params.require(:dose).permit(:description, :ingredient_id)
+end
 end
